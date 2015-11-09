56da567619d3bde981ce8782b553cc68e9491fd3
AlexandTseten <AlexandTseten@appacademy.io>
2015-11-09 14:36:54 -0500

    Add weather widget.

--- autocomplete.js
+++ autocomplete.js
@@ -1,17 +1,26 @@
 var AutoComplete = React.createClass ({
   getInitialState: function (){
-    return { results: [] };
+    return { search: "" };
   },
 
   render: function () {
-    var results = this.state.results;
+    var searchResults = [];
+    for (var i = 0; i < this.props.names.length; i++) {
+      var check = this.props.names[i].slice(0,this.state.search.length);
+      if (check === this.state.search) {
+        searchResults.push(this.props.names[i]);
+      }
+    }
+
+    if (this.state.search === "") { searchResults = []; }
+
     var liNames = [];
-    for (var i = 0; i < results.length; i++){
-      liNames.push(<li>{results[i]}</li>);
+    for (var i = 0; i < searchResults.length; i++){
+      liNames.push(<li onClick={ this.setQuery }>{searchResults[i]}</li>);
     }
     return (
       <div>
-        <input onChange={this.search} type="text"/>
+        <input onChange={this.setSearch} type="text" value={this.state.search}/>
         <ul>
           {liNames}
         </ul>
@@ -19,20 +28,14 @@ var AutoComplete = React.createClass ({
     );
   },
 
-  search: function (e) {
-    if(typeof e === 'undefined'){
-      return [];
-    }
+  setQuery: function (e) {
+    var query = e.currentTarget.innerHTML;
+    this.setState({search: query});
+  },
+
+  setSearch: function (e) {
     var searchQuery = e.currentTarget.value;
-    var searchResults = [];
-    for (var i = 0; i < this.props.names.length; i++) {
-      var check = this.props.names[i].slice(0,searchQuery.length);
-      if (check === searchQuery) {
-        searchResults.push(this.props.names[i]);
-      }
-    }
-    this.setState({results: searchResults});
-    this.render;
+    this.setState({search: searchQuery});
   }
 
 });
---
+++ weather-clock.js
@@ -0,0 +1,90 @@
+var Clock = React.createClass ({
+  getInitialState: function (){
+    return { time: new Date () };
+  },
+
+  render: function () {
+    // debugger;
+    return (
+      <div>
+        {this.state.time.toString()}
+      </div>
+    );
+  },
+
+  componentDidMount: function () {
+    this.handle = setInterval(this.resetDate, 20);
+  },
+
+  componentWillUnmount: function () {
+    clearInterval(this.handle);
+    this.handle = 0;
+  },
+
+  resetDate: function () {
+    this.setState({time: new Date ()});
+  }
+
+});
+
+var Weather = React.createClass({
+
+  locationError: function error(err) {
+    console.warn('ERROR(' + err.code + '): ' + err.message);
+  },
+
+  locationSuccess: function(pos){
+    var crd = pos.coords;
+    this.queryWeather(pos);
+  },
+
+  getInitialState: function (){
+    return { location: navigator.geolocation.getCurrentPosition(this.locationSuccess, this.locationError), temp: 0, weather: ""};
+  },
+
+  queryWeather: function (pos) {
+    var urlStr = "http://api.openweathermap.org/data/2.5/find?lat=" +
+    pos.coords.latitude + "&lon=" + pos.coords.longitude + "&cnt=10" +
+    "&APPID=2de143494c0b295cca9337e1e96b00e0";
+
+    var request = new XMLHttpRequest();
+    request.open('GET', urlStr, true);
+
+    request.onload = function() {
+      if (request.status >= 200 && request.status < 400) {
+        // Success!
+        var resp = JSON.parse(request.responseText);
+        var weatherStr = resp.list[0].weather[0].description;
+        var tempStr = resp.list[0].main.temp;
+        this.setState({ weather: weatherStr, temp: tempStr });
+      } else {
+        // We reached our target server, but it returned an error
+
+      }
+    }.bind(this);
+
+    request.onerror = function() {
+      // There was a connection error of some sort
+    };
+
+    request.send();
+  },
+  // componentDidMount: function(){
+  //   this.setState({ location: navigator.geolocation.getCurrentPosition(this.locationSuccess, this.locationError)});
+  //   console.log(this.state.location);
+  // },
+
+  render: function () {
+    return (
+      <div>
+        <h1>Weather: {this.state.weather}</h1>
+        <h2>Temp: {this.state.temp}</h2>
+      </div>
+    );
+  },
+
+
+
+
+
+});
